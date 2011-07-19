eaedcadaa7c92701337cb56ffa7d9be3045c924c
Geoffrey Bachelet <grosfrais@gmail.com>
2011-07-19 19:18:20 +0200

    updated bootstrap

--- src/app.php
+++ src/app.php
@@ -2,6 +2,12 @@
 
 $app = require_once __DIR__.'/bootstrap.php';
 
+$app->before(function() use ($app) {
+  if (!isset($app['crud.entity'])) {
+    throw new \RuntimeException('Configuration entry "crud.entity" is not set');
+  }
+});
+
 $app->get('/', function() {
   return 'hello, world';
 });
--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,7 +1,54 @@
 <?php
 
 require_once __DIR__.'/../vendor/silex/autoload.php';
+require_once __DIR__.'/../vendor/SilexExtensions/DoctrineMongoDB/src/autoload.php';
 
 $app = new Silex\Application();
+
+$app = new Silex\Application();
+$app['autoloader']->registerNamespaces(array(
+  'Symfony'  => __DIR__.'/../vendor',
+  'Document' => __DIR__,
+  'Form'     => __DIR__,
+));
+
+use Silex\Extension\SymfonyBridgesExtension;
+use Silex\Extension\UrlGeneratorExtension;
+use Silex\Extension\TwigExtension;
+use Silex\Extension\DoctrineMongoDBExtension;
+use Silex\Extension\FormExtension;
+use Silex\Extension\TranslationExtension;
+
+$app->register(new SymfonyBridgesExtension());
+$app->register(new UrlGeneratorExtension());
+
+$app->register(new FormExtension());
+$app->register(new TranslationExtension(), array(
+  'translator.messages' => array()
+));
+
+$app->register(new TwigExtension(), array(
+  'twig.path' => array(__DIR__.'/templates', __DIR__.'/../vendor/Symfony/Bridge/Twig/Resources/views/Form'),
+  'twig.class_path' => __DIR__.'/../vendor/silex/vendor/twig/lib',
+));
+
+$app->register(new DoctrineMongoDBExtension, array(
+  'doctrine.odm.mongodb.connection_options' => array(
+    'database' => 'springbok-silex',
+    'host'     => 'localhost',
+  ),
+  'doctrine.odm.mongodb.documents' => array(
+    array('type' => 'xml', 'path' => __DIR__.'/Document', 'namespace' => 'Document'),
+  ),
+  'doctrine.odm.mongodb.metadata_cache' => 'array',
+  'doctrine.common.class_path'          => __DIR__.'/../vendor/mongodb-odm/lib/vendor/doctrine-common/lib',
+  'doctrine.mongodb.class_path'         => __DIR__.'/../vendor/mongodb-odm/lib/vendor/doctrine-mongodb/lib',
+  'doctrine.odm.mongodb.class_path'     => __DIR__.'/../vendor/mongodb-odm/lib',
+));
+
+$app['doctrine.odm.mongodb.hydrators_dir'] = __DIR__.'/hydrators';
+$app['doctrine.odm.mongodb.configuration']->setDefaultDB('springbok-silex');
+
+require_once __DIR__.'/config.php';
 
 return $app;
